9c888907d79c7d64865584b48b29f482cf79dda4
mursaleen123 <bthunder418@gmail.com>
2023-02-07 23:13:40 +0500

    Update PageRoutes.jsx

--- src/Components/PageRoutes.jsx
+++ src/Components/PageRoutes.jsx
@@ -6,13 +6,9 @@ import Result from "./Results";
 export default function PageRoutes() {
   return (
     <div className="p-4">
-      
       <Routes>
-        <Route path="/" element={<Result />}>
-          <Route index element={<Result />} />
-          <Route path="blogs" element={<Result />} />
-          <Route path="contact" element={<Result />} />
-          <Route path="*" element={<Result />} />
+        <Route path="/">
+          <Result />
         </Route>
       </Routes>
     </div>
